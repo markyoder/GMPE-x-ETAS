353454535a0fac0583be285880a6d385dd0fe0bb
Mark Yoder <mark.yoder@gmail.com>
2016-05-04 22:55:58 -0500

    couple of tweaks to etas_to_gmp interpolation script.

--- etas_to_gmp.py
+++ etas_to_gmp.py
@@ -241,7 +241,7 @@ def interpolation_test_2():
 	#
 	return interpolate2(dtas, (180,360), 1., 360., 1., 180.)
 
-def interpolate2(data,sz, lon1=None, lon2=None, lat1=None, lat2=None):
+def interpolate2(data,sz, lon1=None, lon2=None, lat1=None, lat2=None, fignum=None):
 	# this needs some tuning, but it appears to be working and semi-functional
 	#
 	lons = sorted(list(set([x for x,y,z in data])))
@@ -263,7 +263,7 @@ def interpolate2(data,sz, lon1=None, lon2=None, lat1=None, lat2=None):
 
 	# We need to set up the interpolator object
 
-	from scipy.interpolate import RectSphereBivariateSpline
+	#from scipy.interpolate import RectSphereBivariateSpline
 	lut = RectSphereBivariateSpline(lats, lons, data)
 
 	# Finally we interpolate the data.  The `RectSphereBivariateSpline` object
@@ -274,46 +274,46 @@ def interpolate2(data,sz, lon1=None, lon2=None, lat1=None, lat2=None):
 
 	# Looking at the original and the interpolated data, one can see that the
 	# interpolant reproduces the original data very well:
+	#
+	if fignum!=None:
+		fig = plt.figure()
+		ax1 = fig.add_subplot(211)
+		ax1.imshow(data, interpolation='nearest')
+		ax2 = fig.add_subplot(212)
+		ax2.imshow(data_interp, interpolation='nearest')
+		plt.show()
+		#
+		# Chosing the optimal value of ``s`` can be a delicate task. Recommended
+		# values for ``s`` depend on the accuracy of the data values.  If the user
+		# has an idea of the statistical errors on the data, she can also find a
+		# proper estimate for ``s``. By assuming that, if she specifies the
+		# right ``s``, the interpolator will use a spline ``f(u,v)`` which exactly
+		# reproduces the function underlying the data, she can evaluate
+		# ``sum((r(i,j)-s(u(i),v(j)))**2)`` to find a good estimate for this ``s``.
+		# For example, if she knows that the statistical errors on her
+		# ``r(i,j)``-values are not greater than 0.1, she may expect that a good
+		# ``s`` should have a value not larger than ``u.size * v.size * (0.1)**2``.
 
-	import matplotlib.pyplot as plt
-	fig = plt.figure()
-	ax1 = fig.add_subplot(211)
-	ax1.imshow(data, interpolation='nearest')
-	ax2 = fig.add_subplot(212)
-	ax2.imshow(data_interp, interpolation='nearest')
-	plt.show()
+		# If nothing is known about the statistical error in ``r(i,j)``, ``s`` must
+		# be determined by trial and error.  The best is then to start with a very
+		# large value of ``s`` (to determine the least-squares polynomial and the
+		# corresponding upper bound ``fp0`` for ``s``) and then to progressively
+		# decrease the value of ``s`` (say by a factor 10 in the beginning, i.e.
+		# ``s = fp0 / 10, fp0 / 100, ...``  and more carefully as the approximation
+		# shows more detail) to obtain closer fits.
 
-	# Chosing the optimal value of ``s`` can be a delicate task. Recommended
-	# values for ``s`` depend on the accuracy of the data values.  If the user
-	# has an idea of the statistical errors on the data, she can also find a
-	# proper estimate for ``s``. By assuming that, if she specifies the
-	# right ``s``, the interpolator will use a spline ``f(u,v)`` which exactly
-	# reproduces the function underlying the data, she can evaluate
-	# ``sum((r(i,j)-s(u(i),v(j)))**2)`` to find a good estimate for this ``s``.
-	# For example, if she knows that the statistical errors on her
-	# ``r(i,j)``-values are not greater than 0.1, she may expect that a good
-	# ``s`` should have a value not larger than ``u.size * v.size * (0.1)**2``.
+		# The interpolation results for different values of ``s`` give some insight
+		# into this process:
 
-	# If nothing is known about the statistical error in ``r(i,j)``, ``s`` must
-	# be determined by trial and error.  The best is then to start with a very
-	# large value of ``s`` (to determine the least-squares polynomial and the
-	# corresponding upper bound ``fp0`` for ``s``) and then to progressively
-	# decrease the value of ``s`` (say by a factor 10 in the beginning, i.e.
-	# ``s = fp0 / 10, fp0 / 100, ...``  and more carefully as the approximation
-	# shows more detail) to obtain closer fits.
-
-	# The interpolation results for different values of ``s`` give some insight
-	# into this process:
-
-	fig2 = plt.figure()
-	s = [3e9, 2e9, 1e9, 1e8]
-	for ii in range(len(s)):
-		lut = RectSphereBivariateSpline(lats, lons, data, s=s[ii])
-		data_interp = lut.ev(new_lats.ravel(),
-		                     new_lons.ravel()).reshape((360, 180)).T
-		ax = fig2.add_subplot(2, 2, ii+1)
-		ax.imshow(data_interp, interpolation='nearest')
-		ax.set_title("s = %g" % s[ii])
+		fig2 = plt.figure(fignum+1)
+		s = [3e9, 2e9, 1e9, 1e8]
+		for ii in range(len(s)):
+			lut = RectSphereBivariateSpline(lats, lons, data, s=s[ii])
+			data_interp = lut.ev(new_lats.ravel(),
+				                 new_lons.ravel()).reshape((360, 180)).T
+			ax = fig2.add_subplot(2, 2, ii+1)
+			ax.imshow(data_interp, interpolation='nearest')
+			ax.set_title("s = %g" % s[ii])
 	plt.show()
 #
 def calc_GMPEs(ETAS_rec=None, lat_range=None, lon_range=None, m_reff=5.0, motion_type="PGA-soil", just_z=False):
